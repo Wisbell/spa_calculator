57a8c0cb063567c0aa1e8386adc40d90b7dcfe2e
Wisbell <westleyisbell@gmail.com>
2016-12-13 17:10:04 -0600

    Implemented perform operation function

--- calculator.js
+++ calculator.js
@@ -3,11 +3,19 @@
   passed in as arguments. Return the product.
  */
 
+function multiply(number1, number2){
+  return number1 * number2
+}
 
 /*
   Create a function that adds two numbers
   passed in as arguments. Return the sum.
  */
+
+function add(number1, number2){
+  return number1 + number2
+}
+
 
 
 /*
@@ -15,12 +23,20 @@
   passed in as arguments. Return the difference.
  */
 
+function subtract(number1, number2){
+  return number1 - number2
+}
+
+
 
 /*
   Create a function that divides two numbers
   passed in as arguments. Return the quotient.
  */
 
+function divide(number1, number2){
+  return number1 / number2
+}
 
 
 /*
@@ -32,27 +48,64 @@
   Return the value of the operation.
  */
 
- function applyEventListeners() {
 
-  document.querySelector('.add').addEventListener('click', function(event){
-    event.preventDefault()
-    console.log("Add button clicked")
-  })
+function performOperation(number1, number2, operation) {
+  console.log("number1", number1)
+  console.log("number1", number2)
+  console.log("operation", operation)
+  return `${operation}(${number1}, ${number2})`
+}
 
-  document.querySelector('.subtract').addEventListener('click', function(event){
-    event.preventDefault()
-    console.log("Subtract button clicked")
-  })
 
-  document.querySelector('.multiply').addEventListener('click', function(event){
-    event.preventDefault()
-    console.log("Multiply button clicked")
-  })
+// Event Listeners
 
-  document.querySelector('.divide').addEventListener('click', function(event){
-    event.preventDefault()
-    console.log("Divide button clicked")
-  })
+
+function applyEventListeners() {
+
+  var buttons = document.querySelectorAll('button');
+  var number1;
+  var number2;
+  var operation;
+
+  for(var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(event){
+      event.preventDefault()
+
+      // Get numbers
+      number1 = document.querySelector('input[name=number1]')
+      number2 = document.querySelector('input[name=number2]')
+
+      // Get operation
+      operation = event.target.classList[0]
+
+      console.log(performOperation(number1, number2, operation))
+      performOperation(number1, number2, operation)
+
+
+
+
+    })
+  }
+
+  // document.querySelector('.add').addEventListener('click', function(event){
+  //   event.preventDefault()
+  //   console.log("Add button clicked")
+  // })
+
+  // document.querySelector('.subtract').addEventListener('click', function(event){
+  //   event.preventDefault()
+  //   console.log("Subtract button clicked")
+  // })
+
+  // document.querySelector('.multiply').addEventListener('click', function(event){
+  //   event.preventDefault()
+  //   console.log("Multiply button clicked")
+  // })
+
+  // document.querySelector('.divide').addEventListener('click', function(event){
+  //   event.preventDefault()
+  //   console.log("Divide button clicked")
+  // })
 
  }
 
